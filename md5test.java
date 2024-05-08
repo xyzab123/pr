c74125f562008d52ab36f266610f7e299f2cd837
xyzab123 <hpsinghpatel77@gmail.com>
2024-05-08 08:18:52 +0530

    Add files via upload

---
+++ md5test.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.math.*;
+import java.security.*;
+
+public class md5{
+
+    public static String md(String s){
+        try{
+            MessageDigest m=MessageDigest.getInstance("MD5");
+            byte[] message=m.digest(s.getBytes());
+            BigInteger n=new BigInteger(1,message);
+            String hashtext=n.toString(16);
+            while(hashtext.length()<32){
+                hashtext="0"+hashtext;
+            }
+            return hashtext;
+        }catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+    public static void main(String [] arg){
+        Scanner sc =new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("hash function of above text is "+md(s));
+    }
+}
